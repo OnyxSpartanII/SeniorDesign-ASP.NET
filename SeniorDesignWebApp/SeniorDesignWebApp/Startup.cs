01556d05359655d42b7374d08a327a90799d6efb
Collin Duncan <cgduncan7@gmail.com>
2014-11-17 07:50:34 -0600

    Added initial project

---
+++ SeniorDesignWebApp/SeniorDesignWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeniorDesignWebApp.Startup))]
+namespace SeniorDesignWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
