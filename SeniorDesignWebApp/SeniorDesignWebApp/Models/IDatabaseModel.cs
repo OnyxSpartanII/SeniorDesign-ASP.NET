8eb5bf848aaa915a0812b90e63982905e586047f
Collin Duncan <cgduncan7@gmail.com>
2014-11-20 17:26:31 -0600

    Added in new sample model and classes.

---
+++ SeniorDesignWebApp/SeniorDesignWebApp/Models/IDatabaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SeniorDesignWebApp.Models
+{
+    public class IDatabaseModel
+    {
+        public int ID { get; set; }
+    }
+
+    public class IDbContext : DbContext
+    {
+        public DbSet<IDatabaseModel> IDatabaseModels { get; set; }
+    }
+}
