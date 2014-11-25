b3c6b1944df6d22dbd10c37a2a6ea55e79a4df89
lwestrom <l.westrom@tcu.edu>
2014-11-25 15:59:47 -0600

    Added db connection and button

---
+++ SeniorDesignWebApp/SeniorDesignWebApp/Controllers/UploadController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeniorDesignWebApp.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            var model = new SeniorDesignWebApp.Models.upload();
+            return View(model);
+        }
+        /** Uploading File feature */
+        [HttpPost]
+        public ActionResult Index(SeniorDesignWebApp.Models.upload model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            byte[] uploadedFile = new byte[model.File.InputStream.Length];
+            model.File.InputStream.Read(uploadedFile, 0, uploadedFile.Length);
+
+            // now you could pass the byte array to your model and store wherever 
+            // you intended to store it
+
+            return Content("Thanks for uploading the file");
+        }
+    }
+}
