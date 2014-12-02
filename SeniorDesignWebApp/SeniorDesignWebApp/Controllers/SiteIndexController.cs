7d1e91c440dc7789f3f0c8b41c6ac11779aad1ce
OnyxSpartanII <davidtomlinson25@gmail.com>
2014-12-02 01:21:35 -0600

    Rough website skeleton
    This is a rough outline of the site at this time. Pages are mostly
    empty. Will collaborate with Brice to work out design.

---
+++ SeniorDesignWebApp/SeniorDesignWebApp/Controllers/SiteIndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeniorDesignWebApp.Controllers
+{
+    public class SiteIndexController : Controller
+    {
+        // GET: SiteIndex
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
