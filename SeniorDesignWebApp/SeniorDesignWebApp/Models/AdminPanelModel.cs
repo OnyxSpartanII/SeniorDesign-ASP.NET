0b47bd74438932b97dffcf02d7c742f52f21a940
OnyxSpartanII <dattcb@gmail.com>
2014-12-04 15:57:15 -0600

    Added admin panel controller

---
+++ SeniorDesignWebApp/SeniorDesignWebApp/Models/AdminPanelModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SeniorDesignWebApp.Models
+{
+    public class AdminPanelModel
+    {
+        [Required]
+        public HttpPostedFileBase File { get; set; }
+    }
+}
